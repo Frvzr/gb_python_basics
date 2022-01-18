8eb8cc129e995771037b90292ffeeba1b53cdeb6
Ivan Koposhilov <ikoposhilov@yandex.ru>
2022-01-18 20:00:37 +0500

    add and completed task_1_3

---
+++ Koposhilov_Ivan_dz_1/task_1_3.py
@@ -0,0 +1,13 @@
+# Реализовать склонение слова процент во фразе N процентов. 
+# Вывести эту фразу на экран отдельной строкой для каждого из чисел в интервале от 1 до 100:
+
+def transform_string(number: int) -> str:
+    if number != 11 and number % 10 == 1:
+        return f'{number} процент'
+    elif (number % 10 == 2 or number % 10 == 3 or number % 10 == 4) and number != 12 and number != 13 and number != 14:
+        return f'{number} процента'
+    else:
+        return f'{number} процентов'
+
+for n in range(1, 101):
+    print(transform_string(n))
