813146c015e881c859f60221c479aafe46098f82
Ivan Koposhilov <ikoposhilov@yandex.ru>
2022-01-20 11:24:02 +0500

    change task_1_1.py - at the beginning of the function body, calculate all variables

--- Koposhilov_Ivan_dz_1/task_1_1.py
+++ Koposhilov_Ivan_dz_1/task_1_1.py
@@ -6,23 +6,20 @@
 
 
 def convert_time(duration: int) -> str:
+    day = duration // 86400
+    hour = (duration % 86400) // 3600
+    minute = ((duration % 86400) % 3600) // 60
+    second = ((duration % 86400) % 3600) % 60
     if duration < 60:
-        return f'{duration} сек'
+        return f'{second} сек'
     elif duration >= 60 and duration < 3600:
-        return f'{duration // 60} мин {duration % 60} сек'
+        return f'{minute} мин {second} сек'
     elif duration >= 3600 and duration < 86400:
-        hour = duration // 3600
-        minute = (duration % 3600) // 60
-        second = (duration % 3600) % 60
         return f'{hour} час {minute} мин {second} сек'
     else:
-        day = duration // 86400
-        hour = (duration % 86400) // 3600
-        minute = ((duration % 86400) % 3600) // 60
-        second = ((duration % 86400) % 3600) % 60
         return f'{day} дн {hour} час {minute} мин {second} сек'
 
 
-duration = 500000
+duration = 380000
 result = convert_time(duration)
 print(result)
