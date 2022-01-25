8459947dde89fb7f6bd7786992deaacedd93b1cd
Ivan Koposhilov <ikoposhilov@yandex.ru>
2022-01-25 14:16:49 +0500

    create task_3_1.py and comleted

---
+++ Koposhilov_Ivan_dz_3/task_3_1.py
@@ -0,0 +1,32 @@
+# Написать функцию num_translate(), переводящую числительные от 0 до 10 c английского на русский язык.
+# Если перевод сделать невозможно, вернуть None.
+
+
+def num_translate(key: str) -> str:
+    """переводит числительное с английского на русский """
+
+    numbers = {
+        'zero': 'ноль',
+        'one': 'один',
+        'two': 'два',
+        'three': 'три',
+        'four': 'четыре',
+        'five': 'пять',
+        'six': 'шесть',
+        'seven': 'семь',
+        'eight': 'восемь',
+        'nine': 'девять',
+        'ten': 'десять'
+    }
+
+    if key in numbers:
+        str_out = numbers[key]
+    else:
+        str_out = None
+
+    return str_out
+
+numbers_eng = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven']
+
+for i in numbers_eng:
+    print(num_translate(i))
